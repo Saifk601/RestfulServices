84e31945a0b5c12bbe4f499dc7ba31a93580b0c3
saifur rahman <saifk601@gmail.com>
2024-11-08 20:58:43 +0530

    first commit

---
+++ src/main/java/in/saifit/rest/WelcomeRestController.java
@@ -0,0 +1,34 @@
+package in.saifit.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	 @GetMapping("/welcome")
+	 public ResponseEntity<String> getWelcomeMsg(){
+		 
+	    String respPayload = "Welcome to saif IT";
+		return new ResponseEntity<>(respPayload , HttpStatus.OK);
+		 
+	 }
+	 
+	 @GetMapping("/greet")
+	 public String getGreetMsg() {
+		 String respPayload = "Good Morning .. !!";
+		 return respPayload;
+	 }
+	 
+	 @GetMapping("/msg")
+	 public ResponseEntity<String> getMassage(@RequestParam("name") String name){
+		 
+		 String respPay = name + "  Welcome to saif it";
+		 
+		 return new ResponseEntity<>(respPay , HttpStatus.OK);
+	 }
+
+}
